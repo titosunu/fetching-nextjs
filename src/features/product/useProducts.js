bf27bdeb1c2fdbae7b8d47e75882940c5db47245
titosunu <titosunu2001@gmail.com>
2024-06-04 19:43:44 +0700

    post

--- src/features/product/useProducts.js
+++ src/features/product/useProducts.js
@@ -1,29 +1,12 @@
-import { useEffect, useState } from "react";
 import { axiosInstance } from "@/lib/axios";
+import { useQuery } from "@tanstack/react-query";
 
 export const useProducts = () => {
-  const [products, setProducts] = useState([]);
-  const [setLoading, setListLoading] = useState(false);
-
-  const fetchProducts = async () => {
-    setListLoading(true);
-    try {
-      setTimeout(async () => {
-        const products = await axiosInstance.get("/products");
-        setProducts(products.data);
-        setListLoading(false);
-      }, 1500);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  useEffect(() => {
-    fetchProducts();
-  }, []);
-
-  return {
-    data: products,
-    setLoading,
-  };
+  return useQuery({
+    queryFn: async () => {
+      const products = await axiosInstance.get("/products");
+      return products;
+    },
+    refetchOnWindowFocus: false,
+  });
 };
